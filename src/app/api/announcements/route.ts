7ef08be25fc8aef8d95c41c728037d14c658bc38
beteab548 <beteabbaynessagne548@gmail.com>
2025-07-15 21:45:12 +0300

    fixed announcemnt api logic

--- src/app/api/announcements/route.ts
+++ src/app/api/announcements/route.ts
@@ -1,12 +1,21 @@
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const announcements = await prisma.announcements.findMany({
-    orderBy: { created_at: "desc" },
-  });
+  try {
+    const announcements = await prisma.announcements.findMany({
+      orderBy: { created_at: "desc" },
+    });
 
-  return NextResponse.json(announcements);
+    return NextResponse.json(announcements);
+  } catch (error) {
+    console.error("Failed to fetch announcements:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
