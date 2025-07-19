20c29bf719461658611397454b7a4944796f3c08
beteab548 <beteabbaynessagne548@gmail.com>
2025-07-19 16:23:07 +0300

    ui fixed

--- src/components/contribution/createNewContribution.tsx
+++ src/components/contribution/createNewContribution.tsx
@@ -158,9 +158,9 @@ export default function CreateNewContribution({
                     {...register("mode")}
                     className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white"
                   >
-                    <option value="Recurring">Recurring</option>
+                    <option value="Recurring">Recurring (Has End Date)</option>
                     <option value="OpenEndedRecurring">
-                      Open Ended Recurring
+                      Open Ended Recurring (No End)
                     </option>
                     <option value="OneTimeWindow">
                       One-Time (Fixed Months)
