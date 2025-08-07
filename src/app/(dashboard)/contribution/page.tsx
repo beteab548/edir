17a5e281f57ca22c29c0f9a476de5aed550af171
beteab548 <beteabbaynessagne548@gmail.com>
2025-08-07 19:42:08 +0300

    updated contribution page

--- src/app/(dashboard)/contribution/page.tsx
+++ src/app/(dashboard)/contribution/page.tsx
@@ -92,7 +92,7 @@ export default async function ContributionPage() {
             </div>
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-              {contributionOptions.map((option) => (
+              {contributionOptions.map((option:any) => (
                 <ClientLink
                   key={option.id}
                   href={`/contribution/${option.name}`}
