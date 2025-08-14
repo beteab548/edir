4578594fbc92c75149ddfb30f9610e82844cfcb7
beteab548 <beteabbaynessagne548@gmail.com>
2025-08-14 11:28:45 +0300

    schedule simulation jumped to 2 month in the future

--- src/lib/services/generateSchedulesForAllMembers.ts
+++ src/lib/services/generateSchedulesForAllMembers.ts
@@ -48,9 +48,9 @@ async function inactivateMember(memberId: number, simulate: boolean = false) {
 export async function generateContributionSchedulesForAllActiveMembers(
   options: GenerateSchedulesOptions = {}
 ) {
-  const { simulate = true, simulationMonths = 1 } = options;
+  const { simulate = true, simulationMonths = 2 } = options;
   const now = simulate
-    ? normalizeToMonthStart(subMonths(new Date(), simulationMonths))
+    ? normalizeToMonthStart(addMonths(new Date(), simulationMonths))
     : normalizeToMonthStart(new Date());
   const currentMonthStart = startOfMonth(now);
   const activeMembers = await prisma.member.findMany({
