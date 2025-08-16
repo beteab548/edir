dd882f04d3e7d1c3c1c985fba0fc5a5f69db9b17
beteab548 <beteabbaynessagne548@gmail.com>
2025-08-16 14:32:07 +0300

    total member count updated on the landing page

--- src/app/api/landingpage/route.tsx
+++ src/app/api/landingpage/route.tsx
@@ -4,7 +4,13 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const members = await prisma.member.count({ where: { status: "Active" } });
+  const members = await prisma.member.count({
+    where: {
+      NOT: {
+        status: { in: ["Deceased", "Left"] },
+      },
+    },
+  });
   const activeSince = new Date("2005-07-19");
   return NextResponse.json({ members, activeSince });
 }
